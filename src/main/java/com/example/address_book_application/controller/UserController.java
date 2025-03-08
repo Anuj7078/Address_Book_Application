17c662236ce491fd7f9bb52313aa8eaa8ec855d9
Anuj Choudhary <anuj.choudhary.2711@gmail.com>
2025-03-08 22:21:28 +0530

    UC7  Lombok for logging committed

--- src/main/java/com/example/address_book_application/controller/UserController.java
+++ src/main/java/com/example/address_book_application/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.address_book_application.controller;
 import com.example.address_book_application.dto.UserDTO;
 import com.example.address_book_application.model.User;
 import com.example.address_book_application.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,44 +12,45 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Slf4j // Logging activate karo
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    // GET all users
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
+        log.info("Fetching all users"); // Info level log
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    // GET user by ID
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        log.debug("Fetching user by ID: {}", id); // Debug level log
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
-    // POST create user
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+        log.info("Creating a new user: {}", userDTO); // Info level log
         return ResponseEntity.ok(userService.createUser(userDTO));
     }
 
-    // POST create multiple users
     @PostMapping("/bulk")
     public ResponseEntity<List<User>> createUsers(@RequestBody List<UserDTO> userDTOList) {
+        log.info("Creating multiple users: {}", userDTOList); // Info level log
         return ResponseEntity.ok(userService.saveAllUsers(userDTOList));
     }
 
-    // PUT update user by ID
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        log.info("Updating user with ID {}: {}", id, userDTO); // Info level log
         return ResponseEntity.ok(userService.updateUser(id, userDTO));
     }
 
-    // DELETE user by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        log.warn("Deleting user with ID: {}", id); // Warn level log
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
